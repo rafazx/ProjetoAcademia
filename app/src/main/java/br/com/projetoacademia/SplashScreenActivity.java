0fca1e47e58b7bd62f4c789598aaa754f43e2891
Pedro Kleiz <pkleiz@hotmail.com>
2017-12-05 03:41:51 -0200

    SplashScreen
    SplashScreen criada com a arte provisória

--- app/src/main/java/br/com/projetoacademia/SplashScreenActivity.java
+++ app/src/main/java/br/com/projetoacademia/SplashScreenActivity.java
@@ -1,13 +1,34 @@
 package br.com.projetoacademia;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.concurrent.Delayed;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Handler handle = new Handler();
+        //getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
+
 }
